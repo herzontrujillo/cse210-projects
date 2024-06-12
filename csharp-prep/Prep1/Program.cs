d96ddb9e67be68b5b9135652e187b22d03108812
herzontrujillo <herzonotrujillo@yahoo.com>
2024-06-11 19:08:35 -0500

    Input and Output name & last name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("C# Activity 2!");
         Console.Write("What is your first name?");
         string first = Console.ReadLine();
         Console.Write("What is your last name?");

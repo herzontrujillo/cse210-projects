c4056e7ee59e8fc522440d16937431167f7ab5b0
herzontrujillo <herzonotrujillo@yahoo.com>
2024-06-15 09:06:16 -0500

    step one compute the sum

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.AccessControl;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        
+        Console.WriteLine("C# Activity 4!");
+
+        List<int> numbers = new List<int>();
+        int choice = -1;
+
+        while (choice != 0)
+        {
+            Console.Write("Plese type any number or 0 to quit: ");
+            string userChoice = Console.ReadLine();
+            choice = int.Parse(userChoice);
+
+            if (choice !=0)
+            {
+                //Add the user choice numbers to the list
+                numbers.Add(choice);
+            }
+            
+        }
+
+            //Adding the numbers
+        int addition = 0;
+        foreach (int number in numbers)
+        {
+            addition += number;
+        }
+            //Print the total
+        Console.WriteLine($"The sum of the numbers is: {addition}");
+       
+    
+    }       
 }

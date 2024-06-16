d9c24244988e4e3ae08df9799f3acb8154527b71
herzontrujillo <herzonotrujillo@yahoo.com>
2024-06-15 21:07:05 -0500

    Functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("C# Activity 5!");     
+        
+        // Call the function 
+        WelcomeMesagge();
+        //Create a variable 
+        string Name = UserName();
+        int Number = UserNumber();
+        int Square = SquareNumber(Number);
+
+        DisplayResult(Name, Square);
     }
+
+    static void WelcomeMesagge()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string UserName()
+    {
+        Console.Write("Please type your name: ");
+        string name =  Console.ReadLine();
+        return name;
+    }
+
+    static int UserNumber()
+    {
+        Console.Write("What is your favorite number? ");
+        int number = int.Parse (Console.ReadLine());
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
+
 }

4e89c1ab1c59f24daf57f0ef33d9d21ece0d5a39
herzontrujillo <herzonotrujillo@yahoo.com>
2024-07-05 22:25:32 -0500

    Fraction

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,41 +1,28 @@
 using System;
 using System.Dynamic;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        public class Fraction
-        {
-            private int _top;
-            private int _bottom;
+        Console.WriteLine("Wellcome to the fraction Program");
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine(fraction1.GetDecimalValue());
+        Console.WriteLine(fraction1.GetFractionString());
 
-            // Create constructo that has no parameters initialize it with 1/1
-            public Fraction()
-            {
-                _top = 1; 
-                _bottom = 1;                
-            }
-
-            public Fraction(int wholeNumber)
-            {
-                _top = wholeNumber;
-                _bottom = 1;
-            }
-
-            public Fraction(int top, int bottom)
-            {
-                _top = top;
-                _bottom = bottom;               
-            }
-            public GetDecimalValue ()
-            {
-
-                return (double)_top / (double)_bottom;
-            }
-        }
-
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine(fraction2.GetDecimalValue());
+        Console.WriteLine(fraction2.GetFractionString());
+        
+        Fraction fraction3 = new Fraction(3, 4);
+        Console.WriteLine(fraction3.GetDecimalValue());
+        Console.WriteLine(fraction3.GetFractionString());
+        
+        Fraction fraction4 = new Fraction(1, 3);
+        Console.WriteLine(fraction4.GetDecimalValue());
+        Console.WriteLine(fraction4.GetFractionString());
 
     }
 }

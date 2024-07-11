f9113bb31896eb113acbd0e427103d17edc47361
herzontrujillo <herzonotrujillo@yahoo.com>
2024-07-10 20:56:58 -0500

    Shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Blue", 4);
+        shapes.Add(s1);
+
+        Rectangle r1 = new Rectangle("Red", 4, 2);
+        shapes.Add(r1);
+
+        Circle c1 = new Circle("Yellow", 4);
+        shapes.Add(c1);
+
+        foreach (Shape i in shapes)
+        {
+            double area = i.GetArea();
+            string color = i.GetColor();
+
+            Console.WriteLine($"The shape has a color {color} an area of {area}");
+        }
     }
 }
